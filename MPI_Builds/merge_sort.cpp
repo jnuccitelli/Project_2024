763ef3552d4201c8ecee3b52e366daf854ce3ada
arielamitr <amitrani@tamu.edu>
2024-10-09 13:07:23 -0500

    updated caliper regions, made script loop

--- MPI_Builds/merge_sort.cpp
+++ MPI_Builds/merge_sort.cpp
@@ -22,6 +22,16 @@
 #define FROM_PARENT 1          /* setting a message type */
 #define FROM_CHILD 2          /* setting a message type */
 
+const char* main_cali = "main";
+const char* data_init_runtime = "data_init_runtime";
+const char* correctness_check = "correctness_check";
+const char* comm = "comm";
+const char* comm_small = "comm_small";
+const char* comm_large = "comm_large";
+const char* comp = "comp";
+const char* comp_small = "comp_small";
+const char* comp_large = "comp_large";
+
 double* combineSortedArrays(double* left, double* right, int leftSize, int rightSize) {
    double* returnArray = new double[leftSize+rightSize];
    int r = 0;
@@ -72,51 +82,97 @@ double* mergeSort(double* arr, int size) {
 
 double* startChildProcesses(int id, int childCount, double* arr, int arrSize) {
    //printf("process %d, children %d\n", id, childCount);
+   CALI_MARK_BEGIN(comp);
+   CALI_MARK_BEGIN(comp_small);
    int leftSize = arrSize/2;
    int rightSize = arrSize - leftSize;
    MPI_Status status;
    double* returnArr;
    int firstChildId = 2*id+1;
    if(childCount == 0) {
+      CALI_MARK_END(comp_small);
+      CALI_MARK_BEGIN(comp_large);
       returnArr = mergeSort(arr, arrSize);
+      CALI_MARK_END(comp_large);
+      CALI_MARK_END(comp);
       //printf("Process %d sorted array: ", id);
       //printArray(returnArr, arrSize);
       return returnArr;
    } else if(childCount == 1) {
+      CALI_MARK_END(comp_small);
       //send the left half of the array and its size to child for sorting
+      CALI_MARK_BEGIN(comm);
+      CALI_MARK_BEGIN(comm_small);
       MPI_Send(&leftSize, 1, MPI_INT, firstChildId, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_small);
+      CALI_MARK_BEGIN(comm_large);
       MPI_Send(arr, leftSize, MPI_DOUBLE, firstChildId, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_large);
+      CALI_MARK_END(comm);
 
       //sort the right half of the array ourselves
+      CALI_MARK_BEGIN(comp_large);
       double* rightSorted = mergeSort(arr+leftSize, rightSize);
+      CALI_MARK_END(comp_large);
+      CALI_MARK_END(comp);
 
       //receive the sorted left half of the array from child
       double leftSorted[leftSize];
+      CALI_MARK_BEGIN(comm);
+      CALI_MARK_BEGIN(comm_large);
       MPI_Recv(&leftSorted, leftSize, MPI_DOUBLE, firstChildId, FROM_CHILD, MPI_COMM_WORLD, &status);
+      CALI_MARK_END(comm_large);
+      CALI_MARK_END(comm);
 
       //combine and return the arrays
+      CALI_MARK_BEGIN(comp);
+      CALI_MARK_BEGIN(comp_large);
       returnArr = combineSortedArrays(leftSorted, rightSorted, leftSize, rightSize);
+      CALI_MARK_END(comp_large);
+      CALI_MARK_END(comp);
       //printf("Process %d sorted array: ", id);
       //printArray(returnArr, arrSize);
 
       delete[] rightSorted;
       return returnArr;
    } else { //2 children
+      CALI_MARK_END(comp_small);
+      CALI_MARK_END(comp);
+
       //send the left half of the array and its size to child for sorting
+      CALI_MARK_BEGIN(comm);
+      CALI_MARK_BEGIN(comm_small);
       MPI_Send(&leftSize, 1, MPI_INT, firstChildId, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_small);
+      CALI_MARK_BEGIN(comm_large);
       MPI_Send(arr, leftSize, MPI_DOUBLE, firstChildId, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_large);
 
       //send the right half of the array and its size to child for sorting
+      CALI_MARK_BEGIN(comm_small);
       MPI_Send(&rightSize, 1, MPI_INT, firstChildId+1, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_small);
+      CALI_MARK_BEGIN(comm_large);
       MPI_Send(arr + leftSize, rightSize, MPI_DOUBLE, firstChildId+1, FROM_PARENT, MPI_COMM_WORLD);
+      CALI_MARK_END(comm_large);
+      CALI_MARK_END(comm);
 
+      //receive the sorted arrays
+      CALI_MARK_BEGIN(comm);
+      CALI_MARK_BEGIN(comm_large);
       double leftSorted[leftSize];
       double rightSorted[rightSize];
       MPI_Recv(&leftSorted, leftSize, MPI_DOUBLE, id+1, FROM_CHILD, MPI_COMM_WORLD, &status);
       MPI_Recv(&rightSorted, leftSize, MPI_DOUBLE, id+2, FROM_CHILD, MPI_COMM_WORLD, &status);
+      CALI_MARK_END(comm_large);
+      CALI_MARK_END(comm);
 
       //combine and return the arrays
+      CALI_MARK_BEGIN(comp);
+      CALI_MARK_BEGIN(comp_large);
       returnArr = combineSortedArrays(leftSorted, rightSorted, leftSize, rightSize);
+      CALI_MARK_END(comp_large);
+      CALI_MARK_END(comp);
       //printf("Process %d sorted array: ", id);
       //printArray(returnArr, arrSize);
       
@@ -139,6 +195,7 @@ int main (int argc, char *argv[])
 {
    int sizeOfArray; //this is an input to the code
    int inputType;
+   std::string inputString;
    if (argc == 3)
    {
        sizeOfArray = atoi(argv[1]);
@@ -160,8 +217,16 @@ int main (int argc, char *argv[])
    MPI_Status status;
    int boredProcesses;
    double* sortedArr;
-   const char* whole_computation = "whole_computation";
    double whole_computation_time;
+
+   if(inputType == RANDOM_INPUT)
+      inputString = "random";
+   if(inputType == SORTED_INPUT)
+       inputString = "sorted";
+   if(inputType == REVERSE_SORTED_INPUT)
+       inputString = "reversed";
+   if(inputType == ONE_PERC_PERMUTED)
+       inputString = "permuted";
 
    // Create caliper ConfigManager object
    cali::ConfigManager mgr;
@@ -171,19 +236,29 @@ int main (int argc, char *argv[])
    MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
    MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
 
-   CALI_MARK_BEGIN(whole_computation);
+   CALI_MARK_BEGIN(main_cali);
    double wholeMasterStart = MPI_Wtime();
 
    for(int n = 0; n < numtasks; n++){
       if(taskid == n) {
          if(taskid == MASTER) { //only our first array initializes the array to be sorted
+            CALI_MARK_END(main_cali);
             printIntro(numtasks, sizeOfArray, inputType);
+            CALI_MARK_BEGIN(main_cali);
+
+            CALI_MARK_BEGIN(comp);
+            CALI_MARK_BEGIN(comp_small);
             int children = getChildCount(n, numtasks);
+            CALI_MARK_END(comp_small);
+            CALI_MARK_END(comp);
+
+            CALI_MARK_BEGIN(data_init_runtime);
             double* toSort = generateArray(sizeOfArray, inputType);
+            CALI_MARK_END(data_init_runtime);
             //printf("****************************\nINITIAL ARRAY: \n");
             //printArray(toSort, sizeOfArray);
             //printf("****************************\n\n");
-
+            CALI_MARK_END(main_cali);
             if(children == 2)
                printf("Process %d sending data to processes %d, %d\n", n, n+1, n+2);
             else if(children == 1)
@@ -191,19 +266,35 @@ int main (int argc, char *argv[])
             else if(children == 0)
                printf("Process %d is leaf node; Calculating...\n", n);
 
+            CALI_MARK_BEGIN(main_cali);
             sortedArr = startChildProcesses(n, children, toSort, sizeOfArray);
          } else {
+            CALI_MARK_BEGIN(comp);
+            CALI_MARK_BEGIN(comp_small);
             int children = getChildCount(n, numtasks);
             int leftChild = 2*n+1;
             
             int parentId = (n + n%2 - 2)/2; //(n-1)/2 if odd, (n-2)/2 if even
             int toSortSize;
+            CALI_MARK_END(comp_small);
+            CALI_MARK_END(comp);
 
             //get array and its size from parent process
+            CALI_MARK_BEGIN(comm);
+            CALI_MARK_BEGIN(comm_small);
             MPI_Recv(&toSortSize, 1, MPI_INT, parentId, FROM_PARENT, MPI_COMM_WORLD, &status);
-            double toSort[toSortSize];
-            MPI_Recv(&toSort, toSortSize, MPI_DOUBLE, parentId, FROM_PARENT, MPI_COMM_WORLD, &status);
+            CALI_MARK_END(comm_small);
+            CALI_MARK_END(comm);
 
+            double toSort[toSortSize];
+
+            CALI_MARK_BEGIN(comm);
+            CALI_MARK_BEGIN(comm_large);
+            MPI_Recv(&toSort, toSortSize, MPI_DOUBLE, parentId, FROM_PARENT, MPI_COMM_WORLD, &status);
+            CALI_MARK_END(comm_large);
+            CALI_MARK_END(comm);
+
+            CALI_MARK_END(main_cali);
             //print some info
             if(children == 2)
                printf("Process %d sending data to processes %d, %d\n", n, leftChild, leftChild+1);
@@ -212,39 +303,53 @@ int main (int argc, char *argv[])
             else if(children == 0)
                printf("Process %d is leaf node; Calculating...\n", n);
 
+            CALI_MARK_BEGIN(main_cali);
             //start child processes and receive sorted array
             double* sorted = startChildProcesses(n, children, toSort, toSortSize);
 
             //send sorted array back to parent process; they already know the size
+            CALI_MARK_BEGIN(comm);
+            CALI_MARK_BEGIN(comm_large);
             MPI_Send(sorted, toSortSize, MPI_DOUBLE, parentId, FROM_CHILD, MPI_COMM_WORLD);
+            CALI_MARK_END(comm_large);
+            CALI_MARK_END(comm);
          }
       }
    }
 
-   CALI_MARK_END(whole_computation);
-   double wholeMasterEnd = MPI_Wtime();
-
-   adiak::init(NULL);
-   adiak::user();
-   adiak::launchdate();
-   adiak::libraries();
-   adiak::cmdline();
-   adiak::clustername();
-   adiak::value("num_procs", numtasks);
-   adiak::value("array_size", sizeOfArray);
-   adiak::value("program_name", "merge_sort");
-   adiak::value("array_datatype_size", sizeof(double));
-
    if(taskid == MASTER) {
+      CALI_MARK_BEGIN(correctness_check);
       if(!verifySorted(sortedArr, sizeOfArray)) {
+         CALI_MARK_END(correctness_check);
+         CALI_MARK_END(main_cali);
          printf("There is a bug! This final array is not sorted.\n");
          exit(-1);
       }
-      printf("Total time taken: %f\n", wholeMasterEnd-wholeMasterStart);
-      //printf("\n****************************\nFINAL ARRAY: \n");
-      //printArray(sortedArr, sizeOfArray);
-      //printf("****************************\n");
+      CALI_MARK_END(correctness_check);
    }
+
+   CALI_MARK_END(main_cali);
+   double wholeMasterEnd = MPI_Wtime();
+
+   if(taskid == MASTER) {
+      printf("Total time taken: %f\n", wholeMasterEnd-wholeMasterStart);
+   }
+
+   adiak::init(NULL);
+   adiak::launchdate();    // launch date of the job
+   adiak::libraries();     // Libraries used
+   adiak::cmdline();       // Command line used to launch the job
+   adiak::clustername();   // Name of the cluster
+   adiak::value("algorithm", "merge"); // The name of the algorithm you are using (e.g., "merge", "bitonic")
+   adiak::value("programming_model", "mpi"); // e.g. "mpi"
+   adiak::value("data_type", "double"); // The datatype of input elements (e.g., double, int, float)
+   adiak::value("size_of_data_type", sizeof(double)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+   adiak::value("input_size", sizeOfArray); // The number of elements in input dataset (1000)
+   adiak::value("input_type", inputString); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
+   adiak::value("num_procs", numtasks); // The number of processors (MPI ranks)
+   adiak::value("scalability", "todo"); // The scalability of your algorithm. choices: ("strong", "weak")
+   adiak::value("group_num", 1); // The number of your group (integer, e.g., 1, 10)
+   adiak::value("implementation_source", "handwritten"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
 
 
    mgr.stop();
